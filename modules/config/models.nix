7b0a63f9e0a74151d345bf304ed542f512f45518
Victor Martinez <knoopx@gmail.com>
2025-08-06 00:36:02 +0200

    refactor(models): ♻️ Clean up commented-out code
    Removed unused commented-out code for llamaServer and vllmServer configurations to improve code readability and maintainability. This helps streamline the model configurations and reduces clutter in the codebase.

--- modules/config/models.nix
+++ modules/config/models.nix
@@ -13,13 +13,6 @@
       ${pkgs.llama-cpp}/bin/llama-server -hf ${name} --ctx-size ${toString context} --port ''${PORT} \
         ${lib.concatStringsSep " " args}
     '';
-    # cmd = ''
-    #   ${pkgs.podman}/bin/podman run --rm --name llama-server --device=nvidia.com/gpu=all --ipc=host -p ''${PORT}:8080 \
-    #     -v /var/cache/llama.cpp/:/root/.cache/llama.cpp/ \
-    #     ghcr.io/ggml-org/llama.cpp:server-cuda \
-    #     -hf ${name} --ctx-size ${toString context} \
-    #     ${lib.concatStringsSep " " args}
-    # '';
   };
 
   vllmServer = {
@@ -28,8 +21,6 @@
     args ? [],
   }: {
     context = context;
-    #     sudo podman run --rm --name vllm --device=nvidia.com/gpu=all -p 8099:8880 -v ~/.cache/huggingface:/root/.cache/huggingface --ipc=host vllm/vllm-openai:latest
-    # openai/gpt-oss-20b
     cmd = ''
       ${pkgs.podman}/bin/podman run --rm --name vllm --device=nvidia.com/gpu=all -p ''${PORT}:8880 \
         -v /var/cache/huggingface/:/root/.cache/huggingface/ \
@@ -40,25 +31,25 @@
 in {
   ai = {
     models = {
-      # "qwen2.5-coder" = llamaServer {
-      #   name = "bartowski/Qwen2.5-Coder-32B-Instruct-GGUF:IQ4_NL";
-      #   context = 32768;
-      #   args = [
-      #     "--cache-type-k q8_0"
-      #     "--cache-type-v q8_0"
-      #     "--flash-attn"
-      #     "--jinja"
-      #     "--metrics"
-      #     "--min-p 0.01"
-      #     "--no-context-shift"
-      #     "--no-mmap"
-      #     "--slots"
-      #     "--temp 0.6"
-      #     "--top-k 40"
-      #     "--top-p 0.95"
-      #     "-ngl 99"
-      #   ];
-      # };
+      "qwen2.5-coder" = llamaServer {
+        name = "bartowski/Qwen2.5-Coder-32B-Instruct-GGUF:IQ4_NL";
+        context = 32768;
+        args = [
+          "--cache-type-k q8_0"
+          "--cache-type-v q8_0"
+          "--flash-attn"
+          "--jinja"
+          "--metrics"
+          "--min-p 0.01"
+          "--no-context-shift"
+          "--no-mmap"
+          "--slots"
+          "--temp 0.6"
+          "--top-k 40"
+          "--top-p 0.95"
+          "-ngl 99"
+        ];
+      };
 
       "qwen3-coder-30b-a3b-instruct" = llamaServer {
         name = "unsloth/Qwen3-Coder-30B-A3B-Instruct-1M-GGUF:IQ4_NL";
